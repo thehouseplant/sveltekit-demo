71a6313248a5ca1425bf3a84e8abafc6b949c602
thehouseplant <sean.collins@outlook.com>
2023-01-14 17:29:59 -0800

    Add more tailwind dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
