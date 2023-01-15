ff611b8293e986fa9af05c15f4a60375c8b381aa
thehouseplant <sean.collins@outlook.com>
2023-01-14 16:01:45 -0800

    Add basic Header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<div id="nav">
+  <a href="/">Top</a>
+  <a href="/new">New</a>
+  <a href="/show">Show</a>
+  <a href="/ask">Ask</a>
+  <a href="/jobs">Jobs</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
   import '../app.css';
+  
+  import Header from '$lib/Header.svelte';
 </script>
 
+<Header />
 <slot></slot>
