ab0214d65e9ab5c1269dc121f429c32a661ebb3c
thehouseplant <sean.collins@outlook.com>
2023-01-14 16:29:25 -0800

    Add fetch logic for HN Top Feed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+  let stories;
+
+  onMount(async () => {
+    const response = await fetch('https://api.hackerwebapp.com/news');
+    const data = await response.json();
+    stories = data;
+  });
+</script>
+
 <h1 class="text-3xl font-bold underline">
-  Hello world!
+  Top
 </h1>
+
+{#if stories === undefined}
+  Loading stories...
+{:else}
+  {#each stories as story}
+    {story.title}<br />
+  {/each}
+{/if}
